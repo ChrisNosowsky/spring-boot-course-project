fc5a7b653ef9ffd3e12e329e3f63e3557a16789c
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-06-20 16:23:06 -0400

    Topic service created for controller to use

--- src/main/java/chris/nosowsky/springboot/topic/TopicController.java
+++ src/main/java/chris/nosowsky/springboot/topic/TopicController.java
@@ -1,21 +1,20 @@
 package chris.nosowsky.springboot.topic;
-
-import java.util.Arrays;
+	
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {  // list gets converted to JSON automatically
-		return Arrays.asList(
-				new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("Java", "Java Framework", "Java Framework Description"),
-				new Topic("Javascript", "Javascript Framework", "Javascript Framework Description")
-				);
+		return topicService.getTopics();
 	}
 
 }
---
+++ src/main/java/chris/nosowsky/springboot/topic/TopicService.java
@@ -0,0 +1,19 @@
+package chris.nosowsky.springboot.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = Arrays.asList(
+				new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+				new Topic("Java", "Java Framework", "Java Framework Description"),
+				new Topic("Javascript", "Javascript Framework", "Javascript Framework Description")
+				);
+	
+	public List<Topic> getTopics() {
+		return topics;
+	}
+}
