8ec0fac27cdb92292c393b710a859892add12f8e
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-08-01 00:55:58 -0400

    another service function for getting topic

--- src/main/java/chris/nosowsky/springboot/topic/TopicController.java
+++ src/main/java/chris/nosowsky/springboot/topic/TopicController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TopicController {
@@ -15,6 +16,11 @@ public class TopicController {
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {  // list gets converted to JSON automatically
 		return topicService.getTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 
 }
